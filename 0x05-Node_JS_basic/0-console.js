6012eb70979e66d8d0a1caeed958cfd53ebd8e35
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-08-20 21:51:56 +0300

    feat: Add displayMessage function to print a message to STDOUT

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * A function which dispaly a message to the STDOUT
+ * @param {String} message the message to be display
+ */
+const displayMessage = (message) => {
+    console.log(message);
+  };
+  
+  module.exports = displayMessage;
