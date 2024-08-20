2fcba739e074815ae69ba9c3a961075ea5eb9d87
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-08-20 21:56:52 +0300

    Refactor displayMessage function to improve code readability

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,9 +1,9 @@
 /**
- * A function which dispaly a message to the STDOUT
- * @param {String} message the message to be display
+ * Prints message to stdout
+ * @param {string} string - message to print
  */
-const displayMessage = (message) => {
-    console.log(message);
-  };
-  
-  module.exports = displayMessage;
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
