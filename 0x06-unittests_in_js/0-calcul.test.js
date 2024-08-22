7d871bd93c87676e64c121bdee97e2671139b388
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-08-22 22:32:02 +0300

    chore: Refactor calculateNumber function for better readability and maintainability

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,13 +1,38 @@
 /**
- * Adds two numbers
- * @param   {number}  a
- * @param   {number}  b
- * @returns {number}  -  The sum of a and b
+ * 0-calculus.js.calculateNumber test suite
  */
-function calculateNumber(a, b) {
-    const numOne = Math.round(a);
-    const numTwo = Math.round(b);
-    return numOne + numTwo;
-  }
-  
-  module.exports = calculateNumber;
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber simple test suite', function() {
+  describe('#Add positive numbers', function() {
+    it('should return 4', function() {
+      assert.equal(calculateNumber(1, 3), 4);
+    });
+    it('should return 5', function() {
+      assert.equal(calculateNumber(1, 3.7), 5);
+    });
+    it('should return 5', function() {
+      assert.equal(calculateNumber(1.2, 3.7), 5);
+    });
+    it('should return 6', function() {
+      assert.equal(calculateNumber(1.5, 3.7), 6);
+    });
+  });
+  describe('#Add negative numbers', function() {
+    it('should return -5', function() {
+      assert.equal(calculateNumber(-1.6, -3), -5);
+    });
+    it('should return -6', function() {
+      assert.equal(calculateNumber(-1.6, -3.6), -6);
+    });
+  });
+  describe('#Add positive and negative numbers', function() {
+    it('should return 2', function() {
+      assert.equal(calculateNumber(-1, 3), 2);
+    });
+    it('should return 1', function() {
+      assert.equal(calculateNumber(-1.6, 3), 1);
+    });
+  });
+});
